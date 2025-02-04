0a6fd92419f7ffcc454a5ab3a2793c337270df59
Aniket Kesarwani <aniket.kesarwani@spit.ac.in>
2025-02-04 16:50:22 +0530

    added output.log and modify script

--- bash_log_parse/script.sh
+++ bash_log_parse/script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo -e "Longest Time Response\n"
-awk '/Started/ {req=$0} 
-    /Completed/ {if ($0 ~ /in ([0-9]+)ms/) {
-        split($0, arr, "in "); 
-        split(arr[2], time, "ms"); 
-        print time[1], req, "@", $0}
-    }' ./logfile.log | sort -nr | head -n1 | cut -d' ' -f2- | sed 's/ @ /\n/'
-
+grep -E 'Started|Completed' ./logfile.log | while read -r line; do
+  if [[ $line == *"Started"* ]]; then
+    req="$line"
+  elif [[ $line == *"Completed"* && $line =~ in\ ([0-9]+)ms ]]; then
+    time=${BASH_REMATCH[1]}
+    echo "$time $req @ $line"
+  fi
+done | sort -nr | head -n1 | cut -d' ' -f2- | sed 's/ @ /\n/'
 
 echo -e "\nUnique Endpoints Response\n"
 grep 'Started' ./logfile.log | cut -d'"' -f2 | sed 's/\?.*//' | sort | uniq -c | sort -nr
